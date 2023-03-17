652c29bf1af159312acb537a6bf3cb34332b77a5
Abe Jellinek <jellinek@berkeley.edu>
2023-03-17 17:07:38 -0600

    Zotero.Utilities.varDump(): Set maxLevel for all Nodes/Windows
    Fixes varDump() practically never returning when called on `document` in the
    browser or on a node from another frame.

--- utilities.js
+++ utilities.js
@@ -1480,6 +1480,10 @@ var Utilities = {
 				level = maxLevel;
 			}
 		}
+		// Do the same for cross-context Node and Window objects
+		if (obj.hasOwnProperty('nodeType') || objType === '[object Window]') {
+			level = maxLevel;
+		}
 
 		// Recursion checking
 		if(!parentObjects) {
