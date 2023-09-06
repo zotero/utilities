b63434715720395f60337dc522a22635ecf11cce
Bogdan Abaev <bogdan@zotero.org>
2023-09-06 15:19:05 -0400

    decode uri components in doi when possible
    Also, attempt to drop last bracket or paren if there
    is an opened one before DOI to not skip and pass existing tests.
    
    Fixes: zotero/zotero#3218

--- test/tests/utilitiesTest.js
+++ test/tests/utilitiesTest.js
@@ -93,13 +93,23 @@ describe("Zotero.Utilities", function() {
 			assert.equal(cleanDOI(`Foo bar ${doi}. Foo bar`), doi);
 		});
 		
-		// FIXME
-		it.skip("should parse a DOI in parentheses", function () {
+		it("should parse a DOI with encoded < and >", function () {
+			const encodedUri = "10.1002/1096-9128(200005)12:6%3C375::AID-CPE480%3E3.0.CO;2-M";
+			const expected = "10.1002/1096-9128(200005)12:6<375::AID-CPE480>3.0.CO;2-M";
+			assert.equal(cleanDOI(`Foo bar ${encodedUri}. Foo bar`), expected);
+		});
+
+		it("should parse a DOI with url encoded params", function () {
+			const encodedUri = "https://doi.org/10.1002/1096-9128(200005)12:6%3C375::AID-CPE480%3E3.0.CO;2-M";
+			const expected = "10.1002/1096-9128(200005)12:6<375::AID-CPE480>3.0.CO;2-M";
+			assert.equal(cleanDOI(`Foo bar ${encodedUri}. Foo bar`), expected);
+		});
+		
+		it("should parse a DOI in parentheses", function () {
 			assert.equal(cleanDOI(`Foo bar (${doi}) foo bar`), doi);
 		});
 		
-		// FIXME
-		it.skip("should parse a DOI in brackets", function () {
+		it("should parse a DOI in brackets", function () {
 			assert.equal(cleanDOI(`Foo bar [${doi}] foo bar`), doi);
 		});
 	});
